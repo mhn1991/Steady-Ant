b7f8c86e237caee9bd6062c77239a2e93d07f6e5
Mohammad Hossein Nourian <mhn_1991@yahoo.com>
2020-05-03 13:29:27 +0100

    dominance matrix is finished

--- steady_ant_c/steady_ant.c
+++ steady_ant_c/steady_ant.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#include "linkedlist.h"
-
 
 typedef struct matrixCell{
   int isBlue;
@@ -11,61 +9,56 @@ typedef struct matrixCell{
 }Cell;
 
 
-typedef struct permutationMatrix{
-  // we will need it for steady ant algorithm
-  int isBlue;
-  int *matrix;
-  int size;
-}Matrix;
-
-void makeMatrix(){
-  
+// i row
+// j column
+// n size of the matrix
+int getIndex(int i,int j,int n){
+  return i*n+j;
 }
 
-
-void printArr(int *a,int n) 
-{ 
-  for (int i=0; i<n; i++){ 
-      printf("%d ",a[i]);
+// print our matrix
+void printMatrix(Cell *array,int n){
+  Cell *tmp = array;
+  for(int i=0;i < n; i++){
+    for(int j=0;j < n;j++){
+      printf("%d",tmp[getIndex(i,j,n)].value);
+    }
+    printf("\n");
   }
-    printf("\n"); 
-} 
-  
-// Generating permutation using Heap Algorithm 
-void heapPermutation(int *a, int size, int n,list *l) 
-{
-  if (size == 1) 
-    {
-      append(l,a); 
-      return; 
-    } 
-    
-  for (int i=0; i<size; i++) 
-    { 
-      heapPermutation(a,size-1,n,l); 
-      if (size%2==1){
-	int tmp = a[0];
-	a[0] = a[size-1];
-	a[size-1] = tmp;
-      } 
-      else{
-	int tmp = a[i];
-	a[i] = a[size-1];
-	a[size-1] = tmp;
+}
+
+Cell * dominanceSum(Cell *array,int n){
+  Cell * tmp = array;
+  Cell * new = calloc((n+1)*(n+1),sizeof(Cell));
+  for(int i=0;i < n;i++){
+    for(int j =n-1;j>=0;j--){
+      int sum = 0;
+      for(int k= i;k < n;k++){
+	for(int l= j;l>=0;l--){
+	  sum += tmp[getIndex(k,l,n)].value;
+	}
       }
-    } 
-} 
-   
+      Cell x = {0,sum};
+      new[getIndex(i,j+1,n+1)] = x; 
+      //printf("%d\n",sum);
+    }
+  }
+  printMatrix(new,n+1);
+  return new;
+}
+
+void crossDifference(){
+
+
+}
+
 int main() 
 {
-  int *a = calloc(3,sizeof(int));
-  for(int i=1;i<3;i++){
-    a[i] = i;
-  }
-  list l;
-  init(&l);
-  heapPermutation(a, 3, 3,&l);
-  printArr(l.head->data,3);
-  printf("\n%d\n",l.size);
+  Cell *red = calloc(3*3, sizeof(Cell));
+  Cell redCell = {0,1};
+  Cell blueCell = {1,1};
+  red[1] = red[3] = red[8] = redCell;
+  printMatrix(red,3);
+  dominanceSum(red,3);  
   return 0; 
 } 
